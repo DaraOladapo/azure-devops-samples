23f535f2739da288ddefaef6014333de7a5b680f
Dara Oladapo <dara.oladapo@outlook.com>
2019-07-31 11:12:05 +0100

    Create assign-all-domain-users-to-application.ps1

---
+++ azure/enterprise-apps/assign-all-domain-users-to-application.ps1
@@ -0,0 +1,30 @@
+#install Azure AD Powershell cmdlet module
+Install-Module -Name AzureAD
+
+#Sign user in
+$AzureAdCred = Get-Credential
+Connect-AzureAD -Credential $AzureAdCred
+
+#set search substrings
+$domainSearchString="*@<domainnamehere>"
+$appNameString="<applicationDisplayNameHere>"
+$roleNameString="<roleNameHere>"
+
+$AzureADUsers = Get-AzureADUser -Top 20000 | Where {$_.UserPrincipalName -like $domainSearchString}
+# Get the service principal of the app to assign the user to
+$servicePrincipal = Get-AzureADServicePrincipal -SearchString $appNameString
+
+#iterate over filtered uses
+foreach ($User in  $AzureADUsers){
+    $userName=$User.UserPrincipalName
+    $appName = $appNameString
+    $appRoleName=$roleNameString
+    
+    #get user to assign role
+    $user = Get-AzureADUser -ObjectId "$userName"
+    $sp = Get-AzureADServicePrincipal -Filter "displayName eq '$appName'"
+    $appRole = $sp.AppRoles | Where-Object { $_.DisplayName -eq $appRoleName }
+
+    #Assign the user to the app role
+    New-AzureADUserAppRoleAssignment -ObjectId $user.ObjectId -PrincipalId $user.ObjectId -ResourceId $sp.ObjectId -Id $appRole.Id
+}
